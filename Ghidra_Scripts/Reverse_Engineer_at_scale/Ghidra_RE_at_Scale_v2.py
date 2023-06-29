46c03e403f86d08df11cd927ec24c619c5ba1932
abhijit mohanta <abhijit.mohanta.15.08@gmail.com>
2023-06-29 13:43:37 +0530

    Add files via upload

--- Ghidra_Scripts/Reverse_Engineer_at_scale/Ghidra_RE_at_Scale_v2.py
+++ Ghidra_Scripts/Reverse_Engineer_at_scale/Ghidra_RE_at_Scale_v2.py
@@ -1,4 +1,4 @@
-# a log created for each file with name <filename>_log.tx
+# a log created for each file with name <filename>_scriptName_log.txt in  Logs Directory
 
 import os
 Ghidra_Home = "D:\\Ghidra_test\\Ghidra" #point to you Ghidra home folder
@@ -10,14 +10,17 @@ Project_name = "service" #Ghidra project Name
 SampleDir_path = "D:\\Ghidra_test\\binary\\" #place all samples in this directory
 
 ScriptDir = "D:\\Ghidra_test\\script" #directory containing script
-ScriptName = "list_functions.py" #script name
+
+if not os.path.exists("Logs"):
+	os.makedirs("Logs")
 
 SampleFile_list = os.listdir(SampleDir_path)
 for SampleFile_Name in SampleFile_list:
 	#SampleFile_Name = "Sample_6.1.exe"
 	print "analysis_file => " + str(SampleFile_Name)
+	ScriptName = "list_functions.py" #script name
 	Sample_abs_path = SampleDir_path + str(SampleFile_Name)
-	command = Headless_bat_path + " " + Project_Dir + " " + Project_name + " -import " + Sample_abs_path + " -scriptPath " + ScriptDir + " -postScript " + ScriptName + " -overwrite " + " >> " + str(SampleFile_Name) + "_log.txt"
+	command = Headless_bat_path + " " + Project_Dir + " " + Project_name + " -import " + Sample_abs_path + " -scriptPath " + ScriptDir + " -postScript " + ScriptName + " -overwrite " + " >> " + "Logs\\" +str(SampleFile_Name) + "_" + ScriptName + "_log.txt"
 
 	print command
 	os.system(command)
